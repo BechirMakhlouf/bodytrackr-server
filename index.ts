38a41e5d0e97b983d957570851b37032dacafbbc
bechirMakhlouf <bechirM@proton.me>
2023-07-07 16:56:38 +0100

    add cors middleware

--- index.ts
+++ index.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import jwt from "jsonwebtoken";
-
+import cors from "cors";
 import authRouter from "./src/routes/authRouter.js";
 import userInfoRouter from "./src/routes/userInfoRouter.js";
 
 const PORT = Number(process.env.PORT) || 6969;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
